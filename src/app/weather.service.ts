7af6814a409131577122314055662a13902d231b
Kanrya <nyataraxis@gmail.com>
2017-12-28 23:09:01 +0300

    cleanup
    a minor cleanup of the most mess

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -14,31 +14,16 @@ export class WeatherService {
   constructor(private http:HttpClient) { 
     
   }
-  get data(): Weather[]{
-  	return this.dataChange.value
-  };
-   dataChange: BehaviorSubject<Weather[]> = new BehaviorSubject<Weather[]>([]);
   res: Result;
   queryData: Observable<Result>;
-  queryWeath: Observable<Weather[]>;
-  querl='https://query.yahooapis.com/v1/public/yql?q=select%20item.forecast%20from%20weather.forecast%20where%20woeid%20=%2487889&format=json';
-  
-  getWeathers(cityId:string){
-  	let querl='https://query.yahooapis.com/v1/public/yql?q=select%20item.forecast%20from%20weather.forecast%20where%20woeid%20=%20'+cityId+'&format=json';
+  querl='';
 
-    this.queryData = this.http.get<Result>(querl).pipe(
-    	tap(forecasts => console.log(`fetched forecasts`)));
-    this.querl = querl;
-    this.queryData.subscribe({next:val=>val.query.results.channel.map((el,ind)=>el.item.forecast)});
-    this.dataChange.next(this.data);
-  }
   getForecasts(cityId:string): Observable<Result> {
   	let querl='https://query.yahooapis.com/v1/public/yql?q=select%20item.forecast%20from%20weather.forecast%20where%20woeid%20=%20'+cityId+'&format=json';
 
     this.queryData = this.http.get<Result>(querl).pipe(
     	tap(forecasts => console.log(`fetched forecasts`)));
     this.querl = querl;
-    this.dataChange.next(this.data);
     return this.queryData;
   }
  
--- src/app/weathers/weathers.component.ts
+++ src/app/weathers/weathers.component.ts
@@ -1,14 +1,8 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Weather } from '../Weather';
 import { Result } from '../Result';
 import { FormControl } from '@angular/forms';
-import { WEATHERS } from '../mock-weathers';
 import {MatTableDataSource} from '@angular/material';
-import {DataSource} from '@angular/cdk/collections';
-import { Observable} from 'rxjs/Observable';
-import { Subscription } from 'rxjs/Rx';
-import {merge} from 'rxjs/observable/merge';
-import {HttpClient} from '@angular/common/http';
 
 
 import { WeatherService } from '../weather.service';
@@ -21,99 +15,45 @@ import { WeatherService } from '../weather.service';
 
 
 
-export class WeathersComponent implements AfterViewInit {
-
-  weather: Weather = {
-  	code: 1,
-  	date: "29 Dec 2017",
-  	day: "Fri",
-  	high: 12,
-  	low: -2,
-  	text: "Partly Cloudy"
-  };
+export class WeathersComponent implements OnInit {
 
   weatherArr: Weather[] = [];
-  weathers=WEATHERS;
+
 
   selectedValue: string;
-  private subscription: Subscription = new Subscription();
+
   cities = [
     {value: '2122265', viewValue: 'Moscow'},
     {value: '2459115', viewValue: 'New York'},
     {value: '2345889', viewValue: 'Tokyo'},
     {value: '2487889', viewValue: 'Colorado'}
   ];
+
   startDate = new Date();
+
   lastReq: string;
+
   displayedColumns = ['date', 'day', 'high', 'low', 'text'];
-  /*weatherSource: MyDataSource | null;*/
+
   weatherSource = new MatTableDataSource();
-  exampleDatabase: ExampleHttpDao | null;
-  constructor(private http:HttpClient,private weathService: WeatherService) { }
+
+  constructor(private weathService: WeatherService) { }
   
   changeForecast() {
+
   	this.weatherArr = [];
-  	/*this.weathService.getForecasts(this.selectedValue).subscribe(val =>
-  		val.query.results.channel.map((elem,index)=>this.weatherArr.push(elem.item.forecast)));*/
+
   	this.weathService.getForecasts(this.selectedValue).subscribe(val =>
   		this.weatherSource.data = val.query.results.channel.map((elem,index)=>(elem.item.forecast)));
+
   	console.log(this.weatherArr);
   	this.lastReq = this.weathService.querl;
     
     
   }
+
+  ngOnInit(){
+
+  }
   
-
-  ngAfterViewInit() {
-  	this.exampleDatabase = new ExampleHttpDao(this.http);
-  	merge().pipe(null,)
-
-  }
-
-}
-
-export interface Res {
-  query: QuerRes;
-}
-
-export interface QuerRes {
-	count: number;
-	created: string;
-	lang: string;
-	results: ChannelInterface;
-}
-
-export interface ChannelInterface{
-		channel: WeatherInterface[];
-}
-
-export interface WeatherInterface{
-	item: ForecastInterface;
-}
-
-export interface ForecastInterface{
-	date: string;
-	day: string;
-	high: number;
-	low: number;
-	text:string;
-}
-
-
-export interface GithubIssue {
-  created_at: string;
-  number: string;
-  state: string;
-  title: string;
-}
-export class ExampleHttpDao {
-  constructor(private http: HttpClient) {}
-
-  getRepoIssues(cityId:string): Observable<Res> {
-    const href = 'https://query.yahooapis.com/v1/public/yql';
-    const requestUrl =
-        `${href}?q=select%20item.forecast%20from%20weather.forecast%20where%20woeid%20=%20${cityId}&format=json`;
-
-    return this.http.get<Res>(requestUrl);
-  }
 }
